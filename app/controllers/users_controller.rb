662b10cf6cbf88b09676080233cbfd5495c76cfe
kobakon <138418858+kobakon@users.noreply.github.com>
2023-09-09 07:26:13 +0000

    add ユーザー編集機能、update ユーザー詳細ページ

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,9 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+
   end
-  
+
   def mypage
     @user = current_user
     render 'show'
@@ -10,10 +11,19 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find(params[:id])
-    @post = @user.post
+
   end
-  
+
   def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to mypage_path
   end
-  
+
+  private
+
+  def user_params
+    params.require(:user).permit(:profile_image, :name, :email)
+  end
+
 end
