732063fb781f97ff0853d1b7da3b0be8d0630154
kobakon <138418858+kobakon@users.noreply.github.com>
2023-09-25 14:13:53 +0000

    add テストデータ追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,48 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+admin = User.find_or_create_by!(email: "admin@example.com") do |user|
+  user.name = "管理者"
+  user.password = "kkkkkk"
+  user.profile_image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/48771.png"), filename:"48771.png")
+  user.admin = true
+end
+
+ziro = User.find_or_create_by!(email: "z@z") do |user|
+  user.name = "じろう"
+  user.password = "zzzzzz"
+  user.profile_image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user1.jpg"), filename:"sample-user1.jpg")
+end
+
+taro = User.find_or_create_by!(email: "t@t") do |user|
+  user.name = "たろう"
+  user.password = "tttttt"
+  user.profile_image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user1.jpg"), filename:"sample-user1.jpg")
+end
+
+goro = User.find_or_create_by!(email: "g@g") do |user|
+  user.name = "ごろう"
+  user.password = "gggggg"
+  user.profile_image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user3.jpg"), filename:"sample-user3.jpg")
+end
+
+Image.find_or_create_by!(shop_name: "地球規模で考えろ 伏見本店") do |post|
+  post.image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/地球規模で考えろ.jpg"), filename:"sample-post1.jpg")
+  post.impresions = "ボリューム満点でおいしい！"
+  post.user = ziro
+end
+
+Image.find_or_create_by!(shop_name: "博多ラーメン ShinShin 博多デイトス店") do |post|
+  post.image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/博多ラーメンShinShin博多デイトス店.jpg"), filename:"sample-post1.jpg")
+  post.impresions = "博多ラーメンで一番好き！"
+  post.user = taro
+end
+
+Image.find_or_create_by!(shop_name: "鶏そば啜る 丸の内本店") do |post|
+  post.image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/鶏そば啜る丸の内本店.jpg"), filename:"sample-post1.jpg")
+  post.impresions = "名古屋ラーメンうまかった！"
+  post.user = goro
+end
