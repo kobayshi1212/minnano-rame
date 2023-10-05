c43edb9d7a6f96974c49cc357115f90686efa45c
kobakon <138418858+kobakon@users.noreply.github.com>
2023-10-05 08:04:22 +0000

    update コメントのバリテーション追加

--- app/models/post_comment.rb
+++ app/models/post_comment.rb
@@ -5,6 +5,8 @@ class PostComment < ApplicationRecord
   has_one :notification, as: :subject, dependent: :destroy
   after_create_commit :create_notifications
   
+  validates :comment, presence: true
+  
   private
   def create_notifications
     Notification.create(subject: self, user: self.post.user,action_type: :commented_to_own_post)
